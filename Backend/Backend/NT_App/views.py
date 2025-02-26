174ba83d2e0df0adc91c8a167e24eb10072e4d04
ahmad777x86 <087m.ahmad@gmail.com>
2025-02-26 23:12:53 +0500

    Connected frontend and backend

--- Backend/Backend/NT_App/views.py
+++ Backend/Backend/NT_App/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework import generics
+from rest_framework.decorators import api_view
 from .models import NT_App_Data
 from .serializers import NT_App_DataSerializer
+from rest_framework.response import Response
 
-class add_note(generics.ListCreateAPIView):
-    queryset = NT_App_Data.objects.all()
-    serializer_class = NT_App_DataSerializer
+@api_view(['POST', 'GET'])
+def add_note(request):
+    serializer = NT_App_DataSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors,status=400)
 
 class update_note(generics.RetrieveUpdateDestroyAPIView):
     queryset = NT_App_Data.objects.all()
